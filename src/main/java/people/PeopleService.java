690c728be25991b9f6b146b5ae043fe15cb94934
Djordjije Krivokapic <djolek@amazon.com>
2023-04-18 08:52:06 +0200

    Synchronized list in PeopleService

--- src/main/java/people/PeopleService.java
+++ src/main/java/people/PeopleService.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
@@ -21,7 +22,7 @@ public class PeopleService extends PeopleServiceGrpc.PeopleServiceImplBase {
 
 
     public PeopleService() {
-        this.people = new ArrayList<>(100);
+        this.people = Collections.synchronizedList(new ArrayList<>(100));
     }
 
 
